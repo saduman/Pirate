884aeff702116e1cf743eda356b25d4b2baefe3d
saduman <ugurdag4@gmail.com>
2017-04-24 16:09:32 -0400

    map

--- Pirate.py
+++ Pirate.py
@@ -1,8 +1,13 @@
 
-print "Arr, matey"
+print( "Arr, matey")
 for i in range(100):
-  print str(i) + " bottles of rum"
+  print ( str(i) + " bottles of rum")
 
 with open("treasure.txt") as fh:
   for line in fh:
     print(line)
+
+with open('map.txt', 'w') as fh:
+ 	 for i in range(10):
+   		 fh.write("Walk " + str(i) + " paces left.")
+
